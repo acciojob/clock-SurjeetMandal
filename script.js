9c9025d21b3b6976f1ea725c4a35b04e856b597f
SurjeetMandal <surjeetmandal698@gmail.com>
2024-04-14 16:23:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+let displayTime = document.getElementById("timer");
+
+setInterval(
+	function() {
+		let currentTime = new Date();
+		displayTime.innerHTML = currentTime.toLocaleString();
+	},1000
+)
